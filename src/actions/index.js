6a65af1121148d0c3b8bd0581d9eb4d1da2dbcde
Edward <edward.azarevich@gmail.com>
2025-09-29 12:44:22 +0300

    add new middleware for fetch data and filters

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching);
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
+
+export const fetchFilters = (request) => (dispatch) => {
+    dispatch(filtersFetching());
+    request("http://localhost:3001/filters")
+        .then((data) => dispatch(filtersFetched(data)))
+        .catch(() => dispatch(filtersFetchingError()));
+};
+
 export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
@@ -36,7 +50,7 @@ export const filtersFetchingError = () => {
     }
 }
 
-export const activeFilterChanged = (filter) => {
+export const activeFilterChanged = (filter) => (dispatch) => {
     return {
         type: 'ACTIVE_FILTER_CHANGED',
         payload: filter
--- src/store/index.js
+++ src/store/index.js
@@ -1,40 +1,21 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
-import { thunk } from 'redux-thunk';
-
 
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
         return next({
             type: action
-        });
+        })
     }
     return next(action)
 };
 
-
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            })
-        }
-        return oldDispatch(action)
-    }
-    return store;
-}
-
 const store = createStore(
     combineReducers({ heroes, filters }),
     compose(applyMiddleware(thunk, stringMiddleware),
-        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
-    // compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
