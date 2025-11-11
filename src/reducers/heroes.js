2d8c8c675c5ace64e4f561f0b1e20860f03b7ab4
Edward <edward.azarevich@gmail.com>
2025-11-11 21:11:30 +0300

    add comment with legacy createReducer and the last vertion dosen't have this

--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -14,6 +14,26 @@ const initialState = {
     heroesLoadingStatus: 'idle',
 }
 
+// const heroes = createReducer(initialState, {
+//     [heroesFetching]: state => { state.heroesLoadingStatus = 'loading' },
+//     [heroesFetched]: (state, action) => {
+//         state.heroesLoadingStatus = 'idle';
+//         state.heroes = action.payload;
+//     },
+//     [heroesFetchingError]: state => {
+//         state.heroesLoadingStatus = 'error';
+//     },
+//     [heroCreated]: (state, action) => {
+//         state.heroes.push(action.payload);
+//     },
+//     [heroDeleted]: (state, action) => {
+//         state.heroes = state.heroes.filter(item => item.id !== action.payload);
+//     }
+// },
+//     [],
+//     state => state
+// )
+
 const heroes = createReducer(initialState, builder => {
     builder
         .addCase(heroesFetching, state => {
