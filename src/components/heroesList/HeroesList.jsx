e82b6cc3d6dc766df1d6b214837cd85395c5a1ac
Edward <edward.azarevich@gmail.com>
2025-11-14 11:07:27 +0300

    add new func use createAsyncThunk

--- src/components/heroesList/HeroesList.jsx
+++ src/components/heroesList/HeroesList.jsx
@@ -2,7 +2,7 @@ import { useHttp } from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { createSelector } from 'reselect';
+import { createSelector } from '@reduxjs/toolkit';
 
 import { fetchHeroes } from '../../actions';
 import { heroDeleted } from '../heroesList/heroesSlice';
--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,9 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { useHttp } from '../../hooks/http.hook';
 
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle'
 }
+
+const fetchHeroes = createAsyncThunk(
+    'heroes/fetchHeroes',
+    () => {
+        const request = useHttp();
+        return request("http://localhost:3001/heroes");
+    }
+)
 
 const heroesSlice = createSlice({
     name: 'heroes',
