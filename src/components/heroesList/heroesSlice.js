61e70269e074d4b6437ee75da3559ff04b6f3ec6
Edward <edward.azarevich@gmail.com>
2025-11-14 16:09:10 +0300

    add new slice

--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -6,7 +6,7 @@ const initialState = {
     heroesLoadingStatus: 'idle'
 }
 
-const fetchHeroes = createAsyncThunk(
+export const fetchHeroes = createAsyncThunk(
     'heroes/fetchHeroes',
     () => {
         const request = useHttp();
@@ -18,20 +18,24 @@ const heroesSlice = createSlice({
     name: 'heroes',
     initialState,
     reducers: {
-        heroesFetching: state => { state.heroesLoadingStatus = 'loading' },
-        heroesFetched: (state, action) => {
-            state.heroesLoadingStatus = 'idle';
-            state.heroes = action.payload;
-        },
-        heroesFetchingError: state => {
-            state.heroesLoadingStatus = 'error';
-        },
         heroCreated: (state, action) => {
             state.heroes.push(action.payload);
         },
         heroDeleted: (state, action) => {
             state.heroes = state.heroes.filter(item => item.id !== action.payload);
         }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchHeroes.pending, state => { state.heroesLoadingStatus = 'loading' })
+            .addCase(fetchHeroes.fulfilled, (state, action) => {
+                state.heroesLoadingStatus = 'idle';
+                state.heroes = action.payload;
+            })
+            .addCase(fetchHeroes.rejected, state => {
+                state.heroesLoadingStatus = 'error'
+            })
+            .addDefaultCase(() => { })
     }
 });
 
